00c0ab8b2287a6ec326f1546a9ea6cceeb0c7fca
Boris Huang <scps880510@gmail.com>
2021-08-25 20:11:47 +0800

    新增Loading效果及修改程式碼

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <Loading v-model:active="isLoading" />
   <div class="container mt-4">
     <div class="d-flex justify-content-center align-items-center">
       <h1>高雄 YouBike2.0 公共自行車租賃站即時資訊</h1>
@@ -14,10 +15,10 @@
           <iframe height="200" :src="item.map"></iframe>
           <div class="card-body">
             <p class="card-text">
-              站號 : {{ item.sno }} <br>
-              地點 : {{ item.scity }}{{ item.sarea }} {{ item.ar }} <br>
-              總車位 : {{ item.tot }} 格 <br>
-              可還車位 : {{ item.bemp }} 格 <br>
+              站號 : {{ item.sno }} <br />
+              地點 : {{ item.scity }}{{ item.sarea }}{{ item.ar }} <br />
+              總車位 : {{ item.tot }} 格 <br />
+              可還車位 : {{ item.bemp }} 格 <br />
               可借車輛 : {{ item.sbi }} 輛
             </p>
           </div>
@@ -31,21 +32,26 @@
     <!-- {{ filterData[currentPage] }} -->
     <!-- 分頁 -->
     <nav class="d-flex justify-content-center mt-4" aria-label="Page navigation">
-      <ul class="pagination" >
+      <ul class="pagination">
         <li class="page-item">
           <a class="page-link" href="#" aria-label="Previous"
-          @click.prevent="currentPage === 0 ? currentPage = 0 : currentPage -= 1">
+            @click.prevent="currentPage === 0 ? (currentPage = 0) : (currentPage -= 1)"
+          >
             <span aria-hidden="true">&laquo;</span>
           </a>
         </li>
-        <li class="page-item" :class="{active: currentPage === page - 1}"
-          v-for="(page, index) in filterData.length" :key="index">
+        <li class="page-item" :class="{ active: currentPage === page - 1 }"
+        v-for="(page, index) in filterData.length" :key="index">
           <a class="page-link" href="#" @click.prevent="currentPage = page - 1">{{ page }}</a>
         </li>
         <li class="page-item">
           <a class="page-link" href="#" aria-label="Next"
-          @click.prevent="
-          currentPage === filterData.length-1 ? currentPage = filterData.length-1 : currentPage+=1">
+            @click.prevent="
+              currentPage === filterData.length - 1
+              ? (currentPage = filterData.length - 1)
+              : (currentPage += 1)
+            "
+          >
             <span aria-hidden="true">&raquo;</span>
           </a>
         </li>
@@ -64,6 +70,7 @@ export default {
       currentPage: 0,
       locations: [],
       currentLocation: '',
+      isLoading: false,
     };
   },
   methods: {
@@ -73,6 +80,20 @@ export default {
         locations.add(item.sarea);
       });
       this.locations = Array.from(locations);
+    },
+  },
+  watch: {
+    currentPage() {
+      this.isLoading = true;
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 2500);
+    },
+    currentLocation() {
+      this.isLoading = true;
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 2500);
     },
   },
   computed: {
@@ -109,6 +130,10 @@ export default {
       this.data = response.data.data.retVal;
       this.updatedTime = response.data.data.updated_at;
       this.getLocations();
+      this.isLoading = true;
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 2500);
     });
   },
 };
--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue';
 
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
 
 import App from './App.vue';
 
 const app = createApp(App);
 app.use(VueAxios, axios);
+app.component('Loading', Loading);
 app.mount('#app');
